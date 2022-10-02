c1f84c37505bf233812cf40dbadf81691dc76236
LanchenbiKonthoujam <97613330+LanchenbiKonthoujam@users.noreply.github.com>
2022-10-02 20:30:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -21,5 +21,39 @@ function modelloaded(){
 
 function draw(){
     image(video, 0, 0, 300, 290);
-    
+    if(Status != ""){
+        object_detector.detect(video, gotResults);
+        for(i = 0, i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : objects Detected";
+            console.log(objecta.length);
+            fill();
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "%", objects[i].x + objects[i].y +15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i]width, objects[i].height);
+            
+            if(objects[i].label == input_text){
+                video.stop();
+                object_detector.detect(gotResults);
+                document.getElementById("object_found").innerHTMl = input_text + "Found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesiisUtterance(input_text + "Found");
+                synth,.speak(utterThis);
+            }
+            else{
+                document.getElementById("object_found").innerHTML = input_text + "Not Found";
+            }
+        }
+    }
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
